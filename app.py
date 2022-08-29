d897fd67af516841dee80171396d5afdc4bbd8ef
Paul Bina <63666471+paulbina@users.noreply.github.com>
2022-08-29 19:33:35 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from whitenoise import WhiteNoise
+import json
+
+from oanda_api import OandaAPIForFront
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(
+    app.wsgi_app, root="static/", index_file=True, autorefresh=True
+)
+
+@app.route("/kpi_data")
+def get_kpi_data():
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
+        return data
+
+
+@app.route("/price_data/<pair>")
+def get_price_data(pair):
+    data = OandaAPIForFront.pricing_api(pair)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ defs.py
@@ -0,0 +1,11 @@
+API_KEY = "7531a7f6f8c9c68be1096a0799453d18-21d7c31e6dc34961817d12e490d2f014"
+ACCOUNT_ID = "101-012-21874771-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
+
+BUY = 1
+SELL = -1
+NONE = 0
