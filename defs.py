6e49c10edfc7bd5ba785003f99751135a40a6baa
Paul Bina <63666471+paulbina@users.noreply.github.com>
2022-08-30 00:33:43 +0300

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,9 +1,10 @@
-API_KEY = "7531a7f6f8c9c68be1096a0799453d18-21d7c31e6dc34961817d12e490d2f014"
-ACCOUNT_ID = "101-012-21874771-001"
+API_KEY = "xxx"
+ACCOUNT_ID = "xxx"
 OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
 
 SECURE_HEADER = {
-    'Authorization': f'Bearer {API_KEY}'
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
 }
 
 BUY = 1
